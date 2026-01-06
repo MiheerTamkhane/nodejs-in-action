a136f6f361e15dc40f12f1925d452cd53f950d46
Miheer Tamkhane <sarangtamkhane10@gmail.com>
2026-01-06 20:43:16 +0530

    feat: implement user retrieval, update, and deletion with error handling

--- todolist-with-orm-mvc/controllers/user.controller.js
+++ todolist-with-orm-mvc/controllers/user.controller.js
@@ -18,9 +18,54 @@ const createUser = async (req, res) => {
     }
 };
 
-const getUserById = async (req, res) => {};
-const updateUser = async (req, res) => {}
-const deleteUser = async (req, res) => {}
+const getUserById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await db.select().from(usersTable).where(equals(usersTable.id, id)).get();
+        if (!user) {
+            return res.status(404).json({ error: 'User not found.' });
+        }
+        res.status(200).json({
+            status: 'success',
+            data: user,
+            pagination: {}
+        });
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch data.' });
+    }
+};
+const updateUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updatedUser = await db.update(usersTable).set(req.body).where(equals(usersTable.id, id)).returning();
+        if (!updatedUser) {
+            return res.status(404).json({ error: 'User not found.' });
+        }
+        res.status(200).json({
+            status: 'success',
+            data: updatedUser,
+            pagination: {}
+        });
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch data.' });
+    }
+}
+const deleteUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletedUser = await db.delete(usersTable).where(equals(usersTable.id, id)).returning();
+        if (!deletedUser) {
+            return res.status(404).json({ error: 'User not found.' });
+        }
+        res.status(200).json({
+            status: 'success',
+            data: deletedUser,
+            pagination: {}
+        });
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch data.' });
+    }
+}
 
 module.exports = {
     createUser,
